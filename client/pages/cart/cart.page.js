e4b82b331a3ac2fedb42d393daabefddf7abd560
Adiv Ohayon <adivohayon@gmail.com>
2019-01-01 17:17:05 +0200

    feat: cart route setup

---
+++ client/pages/cart/cart.page.js
@@ -0,0 +1,11 @@
+export default {
+	components: {},
+	async asyncData({ params }) {},
+	data() {
+		return {};
+	},
+	mounted() {
+		// const el = document.querySelector('#shelf-1');
+		// console.log('elll', el);
+	},
+};
