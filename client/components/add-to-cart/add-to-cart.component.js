b35f4e136016872f0218feb5c6be0a308ff7fa08
Adiv Ohayon <adivohayon@gmail.com>
2019-01-02 11:22:44 +0200

    feat: add-to-cart adds to state

--- client/components/add-to-cart/add-to-cart.component.js
+++ client/components/add-to-cart/add-to-cart.component.js
@@ -1,12 +1,24 @@
 export default {
 	name: 'add-to-cart',
 	components: {},
-	props: {},
+	props: {
+		sku: {
+			type: Object,
+			default() {
+				return {};
+			},
+		},
+	},
 	data() {
 		return {};
 	},
 	computed: {},
 	created() {},
 	mounted() {},
-	methods: {},
+	methods: {
+		addToCart(sku) {
+			this.$store.commit('cart/add', { ...sku, quantity: 1 });
+			console.log('Add to cart', sku);
+		},
+	},
 };
