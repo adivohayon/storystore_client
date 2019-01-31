57c2fbd43fb0d866457db2f8c2c0e73c4832ebe6
Gilad Novik <gilad@novik.ca>
2019-02-01 00:55:31 +0200

    sni & docker support

--- server/server.js
+++ server/server.js
@@ -1,4 +1,49 @@
+'use strict';
+const https = require('https');
+const tls = require('tls');
+const AWS = require('aws-sdk');
 const app = require('./nuxt');
-const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const PORT = 3000;
+const TableName = 'StoryStoreCertificates';
+
+const table = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' });
+
+const cache = new Map();
+
+setTimeout(() => cache.clear(), 3600000);
+
+https
+	.createServer(
+		{
+			SNICallback: async (host, cb) => {
+				try {
+					let ctx = cache.get(host);
+					if (!ctx) {
+						let { Item } = await table
+							.get({
+								TableName,
+								Key: { host },
+								AttributesToGet: ['cert', 'privkey'],
+								ReturnConsumedCapacity: 'NONE',
+							})
+							.promise();
+						if (!Item) throw new Error(`Missing certificate for ${host}`);
+
+						ctx = tls.createSecureContext({
+							key: Item.privkey,
+							cert: Item.cert,
+						});
+
+						cache.set(host, ctx);
+					}
+					cb(null, ctx);
+				} catch (err) {
+					console.error(err);
+					cb(err);
+				}
+			},
+		},
+		app
+	)
+	.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
