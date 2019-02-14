062b2901f730ad0ff96decd2a32156ace822030f
Adiv Ohayon <adivohayon@gmail.com>
2019-02-14 23:53:43 +0200

    meta

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -32,6 +32,17 @@ export default {
 	layout(ctx) {
 		return ctx.app.isMobile ? 'mobile' : 'desktop';
 	},
+	head() {
+		const storeSlug = this.$store.state.store.slug;
+		const faviconPath =
+			process.env.staticDir + storeSlug + `/${storeSlug}_favicon.png`;
+
+		return {
+			title:
+				this.$store.state.store.name + ' - ' + this.$store.state.store.tagline,
+			link: [{ rel: 'icon', href: faviconPath }],
+		};
+	},
 	data() {
 		return {
 			isMobile: true,
