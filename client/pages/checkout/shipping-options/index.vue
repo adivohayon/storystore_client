b4ae464b81a8e1af39f6a278823893c24a5a0128
Ben Haddad <benisdman@gmail.com>
2019-05-16 17:41:54 +0300

    cart final fixes

--- client/pages/checkout/shipping-options/index.vue
+++ client/pages/checkout/shipping-options/index.vue
@@ -2,7 +2,7 @@
 	<div class="page checkout-page checkout-page--shipping-options">
 		<div class="checkout-page__header">
 			<h3>פרטי משלוח והזמנה</h3>
-			<nuxt-link :to="`/`" class="checkout-page__back">
+			<nuxt-link :to="`/cart`" class="checkout-page__back">
 				<button class="btn">
 					<icon :original="true" name="chevron" width="22" height="22"></icon>
 				</button>
