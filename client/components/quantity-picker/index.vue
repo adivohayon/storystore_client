17577cd3b4ee67e867bbe423958ab0fce6f861db
Adiv Ohayon <adivohayon@gmail.com>
2019-06-16 19:10:58 +0300

    quantity picker setup

---
+++ client/components/quantity-picker/index.vue
@@ -0,0 +1,5 @@
+<template src="./quantity-picker.component.html"></template>
+
+<script src="./quantity-picker.component.js"></script>
+
+<style src="./quantity-picker.component.scss" scoped lang="scss"></style>
---
+++ client/components/quantity-picker/quantity-picker.component.js
@@ -0,0 +1,14 @@
+export default {
+	name: 'quantity-picker',
+	components: {},
+	props: {},
+	data() {
+		return {
+			quantity: 1,
+		};
+	},
+	computed: {},
+	created() {},
+	mounted() {},
+	methods: {},
+};
